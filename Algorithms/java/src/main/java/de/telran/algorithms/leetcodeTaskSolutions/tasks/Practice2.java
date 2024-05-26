1615ccb7ae08fdb7636b4223f2c3579e30ab2a0d
Win-10 <azimovanvarjon777@gmail.com>
2024-05-26 17:02:31 +0200

    solution of task rotateArray()

--- Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/Practice2.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/Practice2.java
@@ -3,9 +3,11 @@ package de.telran.algorithms.leetcodeTaskSolutions.tasks;
 public class Practice2 {
     public static void main(String[] args) {
 
-        int[] nums = {1,3,4,3,1,1,8,1};
+        int[] nums = {1, 0, 0, 1, 5};
         int candidate = majorityElement(nums);
         System.out.println(candidate);
+
+        rotate(nums, 2);
     }
 
 //    TASK - Majority Element
@@ -14,11 +16,30 @@ public class Practice2 {
         int count = 0;
         int candidate = nums[0];
         for (int num : nums) {
-            if (count == 0){
+            if (count == 0) {
                 candidate = num;
             }
             count += (num == candidate) ? 1 : -1;
         }
         return candidate;
+    }
+
+//    TASK - Rotate Array
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
