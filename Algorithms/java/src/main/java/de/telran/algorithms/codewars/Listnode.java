9472854ddacffb3edf798cd8bf911b9ec8e3adac
Win-10 <azimovanvarjon777@gmail.com>
2024-06-20 16:55:08 +0200

    solution of task mergeTwoList

---
+++ Algorithms/java/src/main/java/de/telran/algorithms/codewars/Listnode.java
@@ -0,0 +1,13 @@
+package de.telran.algorithms.codewars;
+
+public class Listnode {
+
+    int val;
+
+    Listnode next;
+
+    public Listnode(int val) {
+        this.val = val;
+    }
+}
+
--- Algorithms/java/src/main/java/de/telran/algorithms/codewars/Practice1.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/codewars/Practice1.java
@@ -7,6 +7,21 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println(convertBinaryArrayToInt(List.of(0,0,0,1)));
 
+        Listnode listnode1 = new Listnode(1);
+        listnode1.next = new Listnode(2);
+        listnode1.next.next = new Listnode(5);
+        Listnode listnode2 = new Listnode(2);
+        listnode2.next = new Listnode(3);
+        listnode2.next.next = new Listnode(4);
+
+        Listnode merged = mergeTwoLists(listnode1, listnode2);
+
+        while (merged != null) {
+            System.out.println(merged.val + " ");
+            merged = merged.next;
+        }
+        System.out.println(mergeTwoLists(listnode1, listnode2));
+
     }
 
     private static int convertBinaryArrayToInt(List<Integer> binaryArray) {
@@ -18,4 +33,29 @@ public class Practice1 {
 
         return result;
     }
+
+    private static Listnode mergeTwoLists(Listnode l1, Listnode l2) {
+
+        Listnode dummy = new Listnode(0);
+        Listnode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            }else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if (l1 != null) {
+            curr.next = l1;
+        } else if (l2 != null) {
+            curr.next = l2;
+        }
+
+        return dummy.next;
+    }
+
 }
