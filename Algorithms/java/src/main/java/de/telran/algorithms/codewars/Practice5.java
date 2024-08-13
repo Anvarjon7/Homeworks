a7a7bb271eb333a9350b186d69d89c5f9e9389d0
Win-10 <azimovanvarjon777@gmail.com>
2024-08-13 14:40:52 +0200

    solution of tasks binarySearch(), findSmallest(),selectionSort()

--- Algorithms/java/src/main/java/de/telran/algorithms/codewars/Practice5.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/codewars/Practice5.java
@@ -1,11 +1,19 @@
 package de.telran.algorithms.codewars;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Practice5 {
     public static void main(String[] args) {
 
         System.out.println(encode("Dini"));
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        System.out.println(binarySearch(arr,5));
+        int[] arr2 = {234,435,34,2,4,532,1};
+
+        System.out.println(findSmallest(arr2));
+
+        System.out.println(Arrays.toString(selectionSort(arr2)));
 
 
     }
@@ -56,5 +64,87 @@ public class Practice5 {
 
         return result.toString();
     }
+
+
+//    private static boolean s(String s){
+//
+//        Stack<Character>
+//    }
+
+    private static int binarySearch(int[] list, int item){
+
+        int low = 0;
+        int high = list.length - 1;
+
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+            int guess = list[mid];
+            if (guess == item){
+                return mid;
+            }
+            if (guess > item){
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private static int findSmallest(int[] arr){
+
+        int smm = arr[0];
+        int ind = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smm){
+                smm = arr[i];
+                ind = i;
+            }
+        }
+        return ind;
+    }
+
+    private static int[] selectionSort(int[] arr){
+
+        for (int i = 0; i < arr.length; i++) {
+            int minInd = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minInd]){
+                    minInd = j;
+                }
+            }
+
+            int temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
