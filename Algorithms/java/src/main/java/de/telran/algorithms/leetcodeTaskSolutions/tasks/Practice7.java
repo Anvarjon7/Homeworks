a3f3154137ecb8a0700926f57ba514f1b103d41c
Win-10 <azimovanvarjon777@gmail.com>
2024-12-06 19:05:23 +0100

    solved leetcode task numberOfSpecialChars()

--- Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/Practice7.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/Practice7.java
@@ -1,12 +1,17 @@
 package de.telran.algorithms.leetcodeTaskSolutions.tasks;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Practice7 {
     public static void main(String[] args) {
 
-        String word = "usa";
+        String word = "Cc";
 
 
         System.out.println(detect(word));
+
+        System.out.println(numberOfSpecialChars(word));
     }
 
 
@@ -15,11 +20,8 @@ public class Practice7 {
             return true;
         } else if (word.equals(word.toLowerCase())) {
             return true;
-        } else if (Character.isUpperCase(word.charAt(0))
-                && word.substring(1).equals(word.substring(1).toLowerCase())) {
-            return true;
-        }
-        return false;
+        } else return Character.isUpperCase(word.charAt(0))
+                && word.substring(1).equals(word.substring(1).toLowerCase());
     }
 
     private static boolean detect(String word) {
@@ -31,4 +33,28 @@ public class Practice7 {
         return (word.equals(capLetters) || word.equals(lowerCaseLetters) || word.equals(firstCap));
     }
 
+
+    private static int numberOfSpecialChars(String word) {
+
+        Set<Character> lower = new HashSet<>();
+        Set<Character> upper = new HashSet<>();
+
+        int count = 0;
+
+        for (char c : word.toCharArray()){
+            if (Character.isLowerCase(c)){
+                lower.add(c);
+            } else if (Character.isUpperCase(c)) {
+                upper.add(Character.toLowerCase(c));
+            }
+        }
+
+        for (char c : lower){
+            if (upper.contains(c)){
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
