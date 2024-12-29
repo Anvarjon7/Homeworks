abce0c4be8df8f0eec4e3c159a110a7a8e5915d9
Win-10 <azimovanvarjon777@gmail.com>
2024-12-29 17:28:19 +0100

    solved leetcode task reverse2kElems()

--- Algorithms/java/src/main/java/de/telran/algorithms/hackerRank/Solution1.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/hackerRank/Solution1.java
@@ -30,6 +30,8 @@ public class Solution1 {
         List<String> ans = fizzBuzz(15);
         System.out.println(Arrays.toString(new List[]{ans}));
 
+        System.out.println(reverseStr("abcdefg", 2));
+
 
     }
 
@@ -65,6 +67,27 @@ public class Solution1 {
         }
 
         return answer;
+    }
+
+    private static String reverseStr(String s, int k) {
+
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < s.length(); i += 2 * k) {
+
+            int left = i;
+            int right = Math.min(i + k - 1, s.length() - 1);
+
+            while (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        return new String(chars);
     }
 
 }
