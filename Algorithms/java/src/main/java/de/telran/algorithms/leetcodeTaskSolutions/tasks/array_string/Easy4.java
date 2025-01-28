1726cbf0453922a6127b3763e72c815f5e31e042
Win-10 <azimovanvarjon777@gmail.com>
2025-01-28 21:59:54 +0100

    solved leetcode tasks

--- Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/array_string/Easy4.java
+++ Algorithms/java/src/main/java/de/telran/algorithms/leetcodeTaskSolutions/tasks/array_string/Easy4.java
@@ -1,5 +1,6 @@
 package de.telran.algorithms.leetcodeTaskSolutions.tasks.array_string;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Easy4 {
@@ -8,60 +9,61 @@ public class Easy4 {
 
         String s = "abcd";
         String t = "abcde";
-        System.out.println(findTheDifference(s,t));
+        System.out.println(findTheDifference(s, t));
 
         System.out.println(judgeCircle("LL"));
-        String[] list1 = {"Shogun","Tapioca Express","Burger King","KFC"};
-        String[] list2 = {"KFC","Shogun","Burger King"};
+        String[] list1 = {"Shogun", "Tapioca Express", "Burger King", "KFC"};
+        String[] list2 = {"KFC", "Shogun", "Burger King"};
 
 
         System.out.println(Arrays.toString(findRestaurant(list1, list2)));
 
         System.out.println(countBinarySubstrings("00110011"));
-        String[] words = {"step","steps","stripe","stepple"};
+        String[] words = {"step", "steps", "stripe", "stepple"};
 
         System.out.println(shortestCompletingWord("1s3 PSt", words));
 
         System.out.println(reverseWords("Mr Ding"));
 
 
-
         System.out.println(Arrays.toString(shortestToChar("loveleetcode", 'e')));
+
+        System.out.println(dayOfYear("2019-01-09"));
 
 
     }
 
-    private static char findTheDifference(String s, String t){
+    private static char findTheDifference(String s, String t) {
 
         int result = 0;
 
-        for (char c : s.toCharArray()){
+        for (char c : s.toCharArray()) {
             result ^= c;
         }
 
-        for (char c : t.toCharArray()){
+        for (char c : t.toCharArray()) {
             result ^= c;
         }
 
         return (char) result;
     }
 
-    private static boolean judgeCircle(String moves){
+    private static boolean judgeCircle(String moves) {
 
         int x = 0;
         int y = 0;
 
-        for (char c : moves.toCharArray()){
-            if (c == 'R'){
+        for (char c : moves.toCharArray()) {
+            if (c == 'R') {
                 x++;
             }
-            if (c == 'L'){
+            if (c == 'L') {
                 x--;
             }
-            if (c == 'U'){
+            if (c == 'U') {
                 y++;
             }
-            if (c == 'D'){
+            if (c == 'D') {
                 y--;
             }
         }
@@ -69,21 +71,22 @@ public class Easy4 {
         return x == 0 && y == 0;
     }
 
-    private static String[] findRestaurant(String[] list1,String[] list2){
+    private static String[] findRestaurant(String[] list1, String[] list2) {
 
         Map<String, Integer> map = new HashMap<>();
         List<String> res = new ArrayList<>();
         int minIndexSum = Integer.MAX_VALUE;
 
         for (int i = 0; i < list1.length; i++) {
-            map.put(list1[i],i);
+            map.put(list1[i], i);
         }
 
         for (int j = 0; j < list2.length; j++) {
-            if (map.containsKey(list2[j])){
+            if (map.containsKey(list2[j])) {
                 int indexSum = j + map.get(list2[j]);
-                if (indexSum < minIndexSum){
-                    res.clear();;
+                if (indexSum < minIndexSum) {
+                    res.clear();
+                    ;
                     res.add(list2[j]);
                     minIndexSum = indexSum;
                 } else if (indexSum == minIndexSum) {
@@ -108,14 +111,13 @@ public class Easy4 {
         return res + Math.min(cur, pre);
     }
 
-    private static String toLowerCase(String s){
+    private static String toLowerCase(String s) {
 
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 65 || s.charAt(i) <= 90){
-                builder.append((char)(s.charAt(i)+32));
-            }
-            else {
+            if (s.charAt(i) >= 65 || s.charAt(i) <= 90) {
+                builder.append((char) (s.charAt(i) + 32));
+            } else {
                 builder.append(s.charAt(i));
             }
         }
@@ -123,21 +125,21 @@ public class Easy4 {
         return builder.toString();
     }
 
-    private static String shortestCompletingWord(String licensePlate,String[] words){
+    private static String shortestCompletingWord(String licensePlate, String[] words) {
 
         Map<Character, Integer> plateLetterCount = new HashMap<>();
 
-        for (char c : licensePlate.toCharArray()){
-            if (Character.isLetter(c)){
+        for (char c : licensePlate.toCharArray()) {
+            if (Character.isLetter(c)) {
                 char lower = Character.toLowerCase(c);
                 plateLetterCount.put(lower, plateLetterCount.getOrDefault(lower, 0) + 1);
             }
         }
         String result = "";
 
-        for (String word : words){
-            if (isCompletingWord(word,plateLetterCount)){
-                if (result.isEmpty() || word.length() < result.length()){
+        for (String word : words) {
+            if (isCompletingWord(word, plateLetterCount)) {
+                if (result.isEmpty() || word.length() < result.length()) {
                     result = word;
                 }
             }
@@ -145,21 +147,21 @@ public class Easy4 {
         return result;
     }
 
-    private static boolean isCompletingWord(String word, Map<Character,Integer> plateLetterCount){
+    private static boolean isCompletingWord(String word, Map<Character, Integer> plateLetterCount) {
 
         Map<Character, Integer> wordLetterCount = new HashMap<>();
 
-        for (char c : word.toCharArray()){
+        for (char c : word.toCharArray()) {
             char lowerChar = Character.toLowerCase(c);
-            if (Character.isLetter(lowerChar)){
-                wordLetterCount.put(lowerChar, wordLetterCount.getOrDefault(lowerChar,0) + 1);
+            if (Character.isLetter(lowerChar)) {
+                wordLetterCount.put(lowerChar, wordLetterCount.getOrDefault(lowerChar, 0) + 1);
             }
         }
 
-        for (Map.Entry<Character,Integer> entry : plateLetterCount.entrySet()){
+        for (Map.Entry<Character, Integer> entry : plateLetterCount.entrySet()) {
             char letter = entry.getKey();
             int requiredCount = entry.getValue();
-            if (wordLetterCount.getOrDefault(letter, 0) < requiredCount){
+            if (wordLetterCount.getOrDefault(letter, 0) < requiredCount) {
                 return false;
             }
         }
@@ -167,7 +169,7 @@ public class Easy4 {
         return true;
     }
 
-    private static String reverseWords(String s){
+    private static String reverseWords(String s) {
 
         String[] words = s.split(" ");
         StringBuilder result = new StringBuilder();
@@ -186,23 +188,50 @@ public class Easy4 {
         int[] answer = new int[ind];
         Arrays.fill(answer, Integer.MAX_VALUE);
 
-        int prev = Integer.MIN_VALUE / 2;;
+        int prev = Integer.MIN_VALUE / 2;
+        ;
         for (int i = 0; i < ind; i++) {
-            if (s.charAt(i) == c){
+            if (s.charAt(i) == c) {
                 prev = i;
             }
             answer[i] = Math.abs(i - prev);
         }
 
         prev = Integer.MAX_VALUE / 2;
-        for (int i = ind; i >= 0; i--) {
-            if (s.charAt(i) == c){
+        for (int i = ind - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
                 prev = i;
             }
             answer[i] = Math.min(answer[i], Math.abs(i - prev));
         }
 
         return answer;
+    }
+
+    private static int dayOfYear(String date) {
+
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+        String[] parts = date.split("-");
+        int day = Integer.parseInt(parts[2]);
+        int month = Integer.parseInt(parts[1]);
+        int year = Integer.parseInt(parts[0]);
+
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        if (isLeap){
+            daysInMonth[1] = 29;
+        }
+
+        int dayOfYear = 0;
+        for (int i = 0; i < month - 1; i++) {
+            dayOfYear += daysInMonth[i];
+        }
+
+        dayOfYear += day;
+
+        return dayOfYear;
     }
 
 }
